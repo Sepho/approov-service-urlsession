5b035cbcbb50421f1616000f6fbc335663a3663c
Javier Cancio <sepholin@gmail.com>
2023-01-09 13:25:34 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,12 +4,20 @@ import PackageDescription
 let releaseTAG = "3.1.0"
 let package = Package(
     name: "ApproovURLSession",
-    platforms: [.iOS(.v13)],
+    platforms: [.iOS(.v10)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "ApproovURLSession",
-            targets: ["ApproovURLSession", "Approov"]),
+            targets: ["ApproovURLSession"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+        .package(
+            url: "https://github.com/approov/approov-ios-sdk.git", 
+            .exact(from: "3.1.0")
+        )
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
